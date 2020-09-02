0d2699a47898083a38683d6d8610c60bda13eef6
assemblyad <61529304+assemblyad@users.noreply.github.com>
2020-09-02 10:43:26 -0600

    Created Project CoreSuite
    Created Project CoreSuite

---
+++ CoreSuite/src/main/java/MyFirstProgram.java
@@ -0,0 +1,23 @@
+//package ru.stqa.pft.sandbox;
+
+public class MyFirstProgram {
+  public static void main(String[] args) {
+    hello("world");
+    hello("user");
+    hello("Alexander");
+    double l = 5;
+    Square s = new Square(5);
+    System.out.println("The square area area with side " + s.l + "=" + s.area());
+
+    Rectangle r = new Rectangle(4,6);
+    System.out.println("The rectangle  area with side " + r.a + " and " + r.b + "=" + r.area());
+
+  }
+
+  public static void hello(String somebody) {
+
+    System.out.println("Hello, " + somebody + "!");
+
+  }
+
+}
